4c1deacd6a49083b9f6f87001b9c0c33bb8433fa
HeyBroooo <mohitkumar4049a@gmail.com>
2023-09-17 16:34:16 +0530

    changes

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function OnboardingScreen() {
@@ -8,3 +8,10 @@ export default function OnboardingScreen() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    }
+})

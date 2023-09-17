104f6489093cc203c0eec7219ff2a48c1cd79314
HeyBroooo <mohitkumar4049a@gmail.com>
2023-09-17 19:18:21 +0530

    changes

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,52 +1,86 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
-import Onboarding from "react-native-onboarding-swiper";
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
+import React from 'react'
+import Onboarding from 'react-native-onboarding-swiper';
+import Lottie from 'lottie-react-native';
+import { useNavigation } from '@react-navigation/native';
+import { setItem } from '../utils/asyncStorage';
+
+const {width, height} = Dimensions.get('window');
 
 export default function OnboardingScreen() {
+    const navigation = useNavigation();
+
+    const handleDone = ()=>{
+        navigation.navigate('Home');
+        setItem('onboarded', '1');
+    }
+
+    const doneButton = ({...props})=>{
+        return (
+            <TouchableOpacity style={styles.doneButton} {...props}>
+                <Text>Done</Text>
+            </TouchableOpacity>
+        )
+        
+    }
   return (
     <View style={styles.container}>
       <Onboarding
-      containerStyles={{paddingHorizontal: 15}}
-        pages={[
-          {
-            backgroundColor: "#fff",
-            image: (
-              <View>
-                <Text>slide1</Text>
-              </View>
-            ),
-            title: "Onboarding",
-            subtitle: "Done with React Native Onboarding Swiper",
-          },
-          {
-            backgroundColor: "#fff",
-            image: (
-              <View>
-                <Text>slide2</Text>
-              </View>
-            ),
-            title: "Onboarding",
-            subtitle: "Done with React Native Onboarding Swiper",
-          },
-          {
-            backgroundColor: "#fff",
-            image: (
-              <View>
-                <Text>slide3</Text>
-              </View>
-            ),
-            title: "Onboarding",
-            subtitle: "Done with React Native Onboarding Swiper",
-          },
-        ]}
-      />
+            onDone={handleDone}
+            onSkip={handleDone}
+            // bottomBarHighlight={false}
+            DoneButtonComponent={doneButton}
+            containerStyles={{paddingHorizontal: 15}}
+            pages={[
+                {
+                    backgroundColor: '#a7f3d0',
+                    image: (
+                        <View style={styles.lottie}>
+                            <Lottie source={require('../assets/animations/boost.json')} autoPlay loop />
+                        </View>
+                    ),
+                    title: 'Boost Productivity',
+                    subtitle: 'Subscribe this channel to boost your productivity level',
+                },
+                {
+                    backgroundColor: '#fef3c7',
+                    image: (
+                        <View style={styles.lottie}>
+                            <Lottie source={require('../assets/animations/work.json')} autoPlay loop />
+                        </View>
+                    ),
+                    title: 'Work Seamlessly',
+                    subtitle: 'Get your work done seamlessly without interruption',
+                },
+                {
+                    backgroundColor: '#a78bfa',
+                    image: (
+                        <View style={styles.lottie}>
+                            <Lottie source={require('../assets/animations/achieve.json')} autoPlay loop />
+                        </View>
+                    ),
+                    title: 'Achieve Higher Goals',
+                    subtitle: 'By boosting your productivity we help you to achieve higher goals',
+                },
+            ]}
+        />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    lottie:{
+        width: width*0.9,
+        height: width
+    },
+    doneButton: {
+        padding: 20,
+        // backgroundColor: 'white',
+        // borderTopLeftRadius: '100%',
+        // borderBottomLeftRadius: '100%'
+    }
+})

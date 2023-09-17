b20d7e8a0b75968a159086ff7139ae5bf548c2e7
HeyBroooo <mohitkumar4049a@gmail.com>
2023-09-17 16:25:13 +0530

    onboarding-screen

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "../screens/homeScreen";
-import { LogBox, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
 
-const Stack = createStackNavigator();
-
-LogBox.ignoreLogs(['non-serializable values were found in the navigation state']);
+const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
-    return (
-        <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name="Home" option={{headerShown: false}}> component={HomeScreen} </Stack.Screen>
-        </Stack.Navigator>
-        </NavigationContainer>
-    );
-    }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}

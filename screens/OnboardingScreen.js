da1e00b79b70ff90b03c714a2ac88a74c075f67b
HeyBroooo <mohitkumar4049a@gmail.com>
2023-09-17 16:52:36 +0530

    adding slides

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,17 +1,52 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import Onboarding from "react-native-onboarding-swiper";
 
 export default function OnboardingScreen() {
   return (
-    <View>
-      <Text>OnboardingScreen</Text>
+    <View style={styles.container}>
+      <Onboarding
+      containerStyles={{paddingHorizontal: 15}}
+        pages={[
+          {
+            backgroundColor: "#fff",
+            image: (
+              <View>
+                <Text>slide1</Text>
+              </View>
+            ),
+            title: "Onboarding",
+            subtitle: "Done with React Native Onboarding Swiper",
+          },
+          {
+            backgroundColor: "#fff",
+            image: (
+              <View>
+                <Text>slide2</Text>
+              </View>
+            ),
+            title: "Onboarding",
+            subtitle: "Done with React Native Onboarding Swiper",
+          },
+          {
+            backgroundColor: "#fff",
+            image: (
+              <View>
+                <Text>slide3</Text>
+              </View>
+            ),
+            title: "Onboarding",
+            subtitle: "Done with React Native Onboarding Swiper",
+          },
+        ]}
+      />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
